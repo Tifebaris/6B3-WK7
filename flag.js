8d710d4e4c709d770541999a84cd34eeca9c11c7
tifebaris <boluwatifetemileyi@gmail.com>
2023-06-08 22:36:54 +0100

    feat:create for my portfolio

---
+++ flag.js
@@ -0,0 +1,20 @@
+let myName = 'Tife'
+const school = 'Univelcity'
+let house = `Magodo`
+const statement = `my name is ${myName} and
+ i work at ${school} and i am from ${house}`
+
+const statement2 = "Mohammed Ali said 'suffer now, and live the rest of your life a champion' "
+
+
+let yName = prompt('Input your name')
+
+
+let age = prompt('What year were you born?')
+
+let statementnew = `Your name is ${yName} and
+ you are ${new Date().getFullYear() - age} years
+  old`
+
+  console.log(statementnew)
+
